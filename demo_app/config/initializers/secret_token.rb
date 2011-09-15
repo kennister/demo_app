6359bc92ab6996e8fb659f6289d80f19ff743262
Kenny Lee <lizhaoming.86@gmail.com>
2011-09-15 18:15:02 +0800

    Initial commit

--- demo_app/config/initializers/secret_token.rb
+++ demo_app/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DemoApp::Application.config.secret_token = 'c630661f844c9c593278b4460166d40ef02095689aede09aeffa7f8527ce4b405100efcbe0a6a112f5782bb37877d59aa5d8d2879fc9fc55d0698d2a983addc8'
+DemoApp::Application.config.secret_token = 'bab796276b68479145ace51bdb1ce6e560cc619d34d06031a6714edc8c7531f6d7a20bc2866b792cefb2f7db1e6771d2a2a7d024489b2b7f43174c4ca1ad027c'
